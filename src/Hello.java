1faa62ed864c9327aa5ecf2763e7d5956e8f6549
Mate Csanyi <mcmatefr@gmail.com>
2020-03-09 15:46:42 +0100

    Master java changed

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-
+// master
         System.out.println("Hello, World!");
         int myFirstNumber= 5;
         int myFirstNumber2= 6;
