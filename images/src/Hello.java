86843019ffb2932db8838eda8e6f4e6f726d27a9
Mate Csanyi <mcmatefr@gmail.com>
2020-03-09 17:15:42 +0100

    Added gitignore

---
+++ images/src/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+// master
+        System.out.println("Hello, World!");
+        int myFirstNumber= 5;
+        int myFirstNumber2= 6;
+        System.out.println(myFirstNumber2);
+    }
+}
